8e03a2c9eba8de62727855a3e2dbfd0e759588df
Emon17 <kishorhaque@gmail.com>
2025-03-19 14:52:25 +0100

    update

--- report.js
+++ report.js
@@ -2,13 +2,13 @@ const newman = require('newman');
 
 newman.run(
   {
-    collection: require('./user management project Copy.postman_collection.json'), // Pointing to local JSON file
-    //environment: require('./user management project Copy.postman_collection.json'), // Pointing to local env file
+    collection: require('./user management project Copy.postman_collection.json'),
+    
     iterationCount: 1,
     reporters: 'htmlextra',
     reporter: {
       htmlextra: {
-        export: './Reports/report.html', // If not specified, the file will be written to `newman/` in the current working directory.
+        export: './Reports/report.html', 
       },
     },
   },
